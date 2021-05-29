3b0c9071bcbbee0ae209c21546969b4352fdcb71
israel-maia <israelmaiasilva12@gmail.com>
2021-05-29 11:52:17 -0300

    projeto calculadora

---
+++ script.js
@@ -0,0 +1,20 @@
+function calcular() {
+var number1 = parseInt(document.getElementById('number1').value);
+var number2 = parseInt(document.getElementById('number2').value);
+var number3 = parseInt(document.getElementById('number3').value);
+document.getElementById('resultado').value = (number1 + number2 + number3);
+}
+
+function multiplicar() {
+var number4 = parseInt(document.getElementById('number4').value);
+var number5 = parseInt(document.getElementById('number5').value);
+var number6 = parseInt(document.getElementById('number6').value);
+document.getElementById('valor').value = (number4 * number5 * number6);
+}
+
+function dividir() {
+    var number7 = parseInt(document.getElementById('number7').value);
+    var number8 = parseInt(document.getElementById('number8').value);
+    var number9 = parseInt(document.getElementById('number9').value); 
+    document.getElementById('results').value = (number7 / number8 / number9);
+}
